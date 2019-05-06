1d0dff22af6016ab428f9b1fd885fbb91c9b0271
João Cerqueira <joao_cerqueira2@hotmail.com>
2019-05-06 12:29:01 +0100

    Add project files.

---
+++ ghosts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ghosts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
