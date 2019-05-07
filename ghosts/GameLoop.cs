84b0fa485a53b6a33027456b99abccc2e18677ff
Filipe Villar <filipevillar7@gmail.com>
2019-05-07 17:52:40 +0100

    criação da classe gameLoop e pequenas alterações no program.cs

---
+++ ghosts/GameLoop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ghosts
+{
+    class GameLoop
+    {
+
+        public GameLoop ()
+        {
+            
+        }
+
+        public void GameSetup(Board board, Renderer renderer, Player player1, Player player2)
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                renderer.Render(board);
+
+                Positions nextMove;
+                if (board.NextTurn == State.X)
+                    nextMove = player1.GetPosition(board);
+                else
+                    nextMove = player2.GetPosition(board);
+
+                if (!board.SetState(nextMove, board.NextTurn))
+                    Console.WriteLine("That is not a legal move.");
+            }
+        }
+    }
+}
--- ghosts/Program.cs
+++ ghosts/Program.cs
@@ -11,20 +11,23 @@ namespace ghosts
             Renderer renderer = new Renderer();
             Player player1 = new Player();
             Player player2 = new Player();
+            GameLoop gameLoop = new GameLoop();
 
-            while (!winChecker.IsDraw(board) && winChecker.Check(board) == State.Undecided)
-            {
-                renderer.Render(board);
+            /*  while (!winChecker.IsDraw(board) && winChecker.Check(board) == State.Undecided)
+              {
+                  renderer.Render(board);
 
-                Positions nextMove;
-                if (board.NextTurn == State.X)
-                    nextMove = player1.GetPosition(board);
-                else
-                    nextMove = player2.GetPosition(board);
+                  Positions nextMove;
+                  if (board.NextTurn == State.X)
+                      nextMove = player1.GetPosition(board);
+                  else
+                      nextMove = player2.GetPosition(board);
 
-                if (!board.SetState(nextMove, board.NextTurn))
-                    Console.WriteLine("That is not a legal move.");
-            }
+                  if (!board.SetState(nextMove, board.NextTurn))
+                      Console.WriteLine("That is not a legal move.");
+              }*/
+            gameLoop.GameSetup(board, renderer, player1, player2);
+        
 
             renderer.Render(board);
             renderer.RenderResults(winChecker.Check(board));
