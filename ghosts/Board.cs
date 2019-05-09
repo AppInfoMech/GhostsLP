87f624531976d92dd1dd4fe78797e1466d39d55c
Bugs <r1ch404ps@gmail.com>
2019-05-09 05:06:23 +0100

    Added Tile.cs. Updated Board.cs to include a Multidimensional Array of Tiles. Updated GDD.txt with «Special Thanks» for outside members who have been a huge help.

--- ghosts/Board.cs
+++ ghosts/Board.cs
@@ -16,6 +16,31 @@ namespace ghosts
 
         public Board()
         {
+            //Tile Array in order to arrange the board
+            Tile[,] tiles = new Tile[5, 5];
+
+            //For loop in order to create a ordered 5 by 5 board
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    tiles[x, y] = new Tile();
+                }
+            }
+
+
+            /*
+             * TILES ARRAY REPRESENTED GRAPHICALLY
+             * 
+             * [0,0] [0,1] [0,2] [0,3] [0,4]
+             * [1,0] [1,1] [1,2] [1,3] [1,4]
+             * [2,0] [2,1] [2,2] [2,3] [2,4]
+             * [3,0] [3,1] [3,2] [3,3] [3,4]
+             * [4,0] [4,1] [4,2] [4,3] [4,4]
+             * 
+            */
+
+
             state = new State[7, 7];
             NextTurn = State.P1;
         }
---
+++ ghosts/Tile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ghosts
+{
+    //class in which the details/characteristics inside of the tiles in the
+    //board will be set.
+    class Tile
+    {
+        public Tile()
+        {
+
+        }
+    }
+}
